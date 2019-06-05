d14446ebdd08877be62379c1ac0c0d86de894754
Malte Thodberg <maltethodberg@Maltes-MacBook-Pro.local>
2019-06-05 13:55:25 +0200

    test commit

--- R/data_documentation.R
+++ R/data_documentation.R
@@ -1,6 +1,6 @@
 #' Collections of R-packages
 #'
-#' Various collections of useful R-packages
+#' Various collections of useful R-packages.
 #'
 #' @format A named list with package names
 "package_collections"
